60dc5057ad6150cde18024211965e4576f124015
FadhilAlif <92931140+FadhilAlif@users.noreply.github.com>
2023-12-07 17:51:26 +0700

    add: Fadhil Alif Portofolio

---
+++ src/components/About/About.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About Me</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Front-End Developer</h3>
+              <p>
+                I am a Junior Front-End developer with a high interest in
+                learning more about web development
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/uiIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI/UX Designer</h3>
+              <p>
+                I have experience design multipe landing pages and apps with
+                Figma
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
